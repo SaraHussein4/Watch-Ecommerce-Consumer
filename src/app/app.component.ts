383bbf6be41e62308564a435343fc78d5c8537af
SaraHussein4 <saraomran12002@gmail.com>
2025-07-05 03:36:55 +0300

    merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
-import { HomeComponent } from './components/home-component/home-component.component';
 import { FooterComponentComponent } from './components/footer-component/footer-component.component';
 import { NavbarComponentComponent } from './components/navbar-component/navbar-component.component';
-import { ProductsComponent } from './components/Products/Products.component';
-import { AddNewProductComponent } from "./components/add-new-product/add-new-product.component";
+
+import { ChatBotComponent } from './components/chat-bot/chat-bot.component';
 @Component({
   selector: 'app-root',
-  imports: [FooterComponentComponent, NavbarComponentComponent, RouterOutlet, RouterModule],
+  imports: [FooterComponentComponent, NavbarComponentComponent, RouterOutlet, RouterModule,ChatBotComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
