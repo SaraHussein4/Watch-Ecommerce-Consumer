47707bbad79859077627b6d2f4ec4bbb359a189d
Aml A Shabana <amlshbana28@gmail.com>
2025-07-05 17:22:13 +0300

    finall edit

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,4 +1,4 @@
-import { Image } from './../models/image.model';
+import { Image } from '../models/image.model';
 import { Product } from './../models/product.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -7,6 +7,7 @@ import { ProductFilter } from '../models/ProductFilter';
 import { Category } from '../models/category.model';
 import { Brand } from '../models/brand.model';
 import { CartItem } from '../models/cart';
+// import { ImageResponse } from '../models/ImageResponse';
 
 @Injectable({
   providedIn: 'root',
@@ -36,7 +37,7 @@ export class ProductService {
       product
     );
   }
-  
+
   //getCategory
   getCategories(): Observable<Category[]> {
     return this.http.get<Category[]>('https://localhost:7071/api/Categories');
@@ -66,12 +67,9 @@ export class ProductService {
   }
 
   //add image
-  addImage(imageData: any): Observable<any[]> {
-    return this.http.post<any[]>(
-      'https://localhost:7071/api/Image',
-      imageData
-    );
-    // return this.http.get<Product[]>(`https://localhost:7071/api/product?brandId=${brandId}`);
+  addImage(imageData: any): Observable<Image> {
+    // return this.http.post<ImageResponse>('https://localhost:7071/api/Image', imageData);
+    return this.http.post<Image>('https://localhost:7071/api/Image', imageData);
   }
 
   addProduct(productData: FormData): Observable<any> {
@@ -83,10 +81,17 @@ export class ProductService {
   deleteProduct(id: number): Observable<any> {
     return this.http.delete(`https://localhost:7071/api/Product/${id}`);
   }
-  getPaginatedProducts(page: number, pageSize: number): Observable<{ items: Product[]; totalCount: number }> {
-  return this.http.get<{ items: Product[]; totalCount: number }>(
-    `https://localhost:7071/api/Product/paged?page=${page}&pageSize=${pageSize}`
-  );
-}
+  getPaginatedProducts(
+    page: number,
+    pageSize: number
+  ): Observable<{ items: Product[]; totalCount: number }> {
+    return this.http.get<{ items: Product[]; totalCount: number }>(
+      `https://localhost:7071/api/Product/paged?page=${page}&pageSize=${pageSize}`
+    );
+  }
+  //delete img
+  deleteImage(imageId: number): Observable<any> {
+    return this.http.delete(`https://localhost:7071/api/Image/${imageId}`);
+  }
 
 }
