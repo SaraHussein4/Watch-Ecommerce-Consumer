49c7383d777ddefa1616078f8bfa15ea688cba00
Aml A Shabana <amlshbana28@gmail.com>
2025-07-08 18:59:21 +0300

    error done

--- src/app/components/add-new-product/add-new-product.component.ts
+++ src/app/components/add-new-product/add-new-product.component.ts
@@ -52,10 +52,10 @@ export class AddNewProductComponent implements OnInit {
       genderCategory: ['', Validators.required],
       waterResistance: [false],
       WarrentyYears: [1, [Validators.min(0)]],
-      colors: ['', Validators.required],  // comma-separated
-      sizes: ['', Validators.required],   // comma-separated
+      colors: ['', Validators.required], // comma-separated
+      sizes: ['', Validators.required], // comma-separated
       productBrandId: ['', Validators.required],
-      categoryId: ['', Validators.required]
+      categoryId: ['', Validators.required],
     });
   }
 
