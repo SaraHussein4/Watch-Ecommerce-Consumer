e7ab11687f4ae41de21a1d1bff3424bf0b300710
AhmedHamdy3 <ahmed195053@feng.bu.edu.eg>
2025-07-15 03:20:32 +0300

    Delivery

--- src/app/components/admin-deliveries/admin-deliveries.component.ts
+++ src/app/components/admin-deliveries/admin-deliveries.component.ts
@@ -6,10 +6,11 @@ import { Governorate } from '../../models/order';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-admin-deliveries',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule,RouterLink],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './admin-deliveries.component.html',
   styleUrls: ['./admin-deliveries.component.css']
 })
@@ -17,14 +18,16 @@ export class AdminDeliveriesComponent implements OnInit {
   deliveries: Delivery[] = [];
   loading = false;
   error: string | null = null;
+  isLoading: boolean = false;
 
   governorates: Governorate[] = [];
 
   constructor(
     private deliveryService: DeliveryService,
     private orderService: OrderService,
-    private fb: FormBuilder
-  ) {}
+    private fb: FormBuilder,
+    private confirmationService: ConfirmationService
+  ) { }
 
   ngOnInit() {
     this.fetchDeliveries();
@@ -58,15 +61,25 @@ export class AdminDeliveriesComponent implements OnInit {
   }
 
   deleteDelivery(id: string) {
-    if (!confirm('Are you sure you want to delete this delivery?')) return;
-    this.deliveryService.deleteDelivery(id).subscribe({
-      next: () => {
-        this.deliveries = this.deliveries.filter(d => d.id !== id);
-      },
-      error: () => {
-        alert('Failed to delete delivery');
+
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete this product?',
+      header: 'Confirm Deletion',
+      icon: 'pi pi-trash',
+      acceptButtonStyleClass: 'p-button-danger',
+      rejectButtonStyleClass: 'p-button-secondary',
+      acceptLabel: 'Delete',
+      rejectLabel: 'Cancel',
+      accept: () => {
+        this.deliveryService.deleteDelivery(id).subscribe({
+          next: () => {
+            this.deliveries = this.deliveries.filter(d => d.id !== id);
+          },
+          error: () => {
+            alert('Failed to delete delivery');
+          }
+        });
       }
     });
   }
-
 }
